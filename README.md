143d63db185860d521602e41544b4611f9c7b99c
Graham Smith <gsmitheidw@gmail.com>
2021-06-17 17:20:26 +0100

    many changes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sms32v50
-Chocolatey package recipe for Microsimulator Software for Students by C Neil Bauers
+Chocolatey package recipe for Microsimulator Software for Students by [Neil Bauers]
+(https://github.com/nbauers)
+
+##Full site: 
+http://www.softwareforeducation.com/
+
+##Features:
+
+- Learn about microprocessors using a simplified 8 bit machine.
+- Write assembly code programs using commands similar to the Intel 8086 family of chips.
+- Run or single step programs and see the CPU registers and RAM changing.
+- Learn about control systems and program simulated input and output peripherals.
+- Learn about polling and interrupts.
+
+
---
+++ sms32v50/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.softwareforeducation.com/sms32v50/smz32v50.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'smz32v50*' 
+  checksum      = '133EA866CAEF9AA98D614954FBF925918E675DF4ED1F6DF98E487B89D15A9F14'
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$target = Join-Path $toolsDir "Digital\$($packageName).exe"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\smz32v50.lnk" -targetPath $target 
+
+# We cannot set a specific dependency for Java Runtime as Oracle JRE, JDK and OpenJDK etc are all possible alternatives
+# In light of this, warn the user:
+Write-Warning "Note: a valid Java Runtime is required such as jre or equivavlent"
