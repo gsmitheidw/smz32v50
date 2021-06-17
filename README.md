e02623f02d76c0b34d9613019a8e63fa745cf749
Graham Smith <gsmitheidw@gmail.com>
2021-06-17 18:01:30 +0100

    Initial nupkg created and tested

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# sms32v50
+# smz32v50
 Chocolatey package recipe for Microsimulator Software for Students by [Neil Bauers]
 (https://github.com/nbauers)
 
-##Full site: 
+## Full site: 
 http://www.softwareforeducation.com/
 
-##Features:
+## Features:
 
 - Learn about microprocessors using a simplified 8 bit machine.
 - Write assembly code programs using commands similar to the Intel 8086 family of chips.
 - Run or single step programs and see the CPU registers and RAM changing.
 - Learn about control systems and program simulated input and output peripherals.
 - Learn about polling and interrupts.
-
-
+- Licenced with GNU General Public License v3.0
---
+++ smz32v50/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.softwareforeducation.com/sms32v50/smz32v50.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'smz32v50*' 
+  checksum      = '133EA866CAEF9AA98D614954FBF925918E675DF4ED1F6DF98E487B89D15A9F14'
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Note spelling of binary is sms32v50 not smz32v50 here:
+$target = Join-Path $toolsDir "sms32v50.exe"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\smz32v50.lnk" -targetPath $target 
+
